562e707f59a67b6f09f9bf7af7fd3ba1de0a3da6
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-08-30 20:49:21 +0100

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -3,8 +3,9 @@
 This is a basic Flask application.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel  # type: ignore
+from typing import Optional
 app = Flask(__name__)
 babel = Babel(app)
 
@@ -24,13 +25,26 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """
+        Returns the best-matching language for the user based on the accepted
+        languages in the request headers and the available languages in
+        the app configuration.
+
+    Returns:
+        str | None: The best-matching language code or None if no match found.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def hello() -> str:
     """
     This function is a route handler for the root URL of the application.
     It renders an HTML template named '0-index.html'.
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
